e9a8ef22874029400eefd57954efe8df4a84982d
unknown <hibary.taiwan@gmail.com>
2023-09-28 02:10:44 +0800

    feat: set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; //swagger
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Car API')
+    .setDescription('The Car API description')
+    .setVersion('1.0')
+    .addTag('Car')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3051);
 }
 bootstrap();
